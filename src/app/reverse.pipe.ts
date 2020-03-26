43c7ea104ea0077dff87e78db13f0ce16d200fe7
unknown <abhaykumar.lodha@gmail.com>
2020-03-26 21:41:05 +0530

    optimize

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -9,9 +9,7 @@ export class ReversePipe implements PipeTransform {
     transform(value: any) {
 
         if (value.length > 0) {
-            let output = '';
-            value.split('').forEach(element => { output = element + output; });
-            return output;
+            return (value.split('').reverse().join(''));
         }
         else {
             return value;
